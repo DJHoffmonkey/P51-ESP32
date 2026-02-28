c596a90116279f06eb6b60237d34abe732675b7a
DJHoffmonkey <david@hoffmonkey.com>
2026-02-28 17:26:47 +0000

    stuff

--- P51-ESP32/sight/sight.ino
+++ P51-ESP32/sight/sight.ino
@@ -40,14 +40,20 @@ bool sessionHasMSP = false, currentlyReceiving = false, showLowBatText = false;
 // --- HARDWARE BOOST ---
 void applyHardwareBoost(bool enable) {
   if (enable) {
-    u8g2_builtin.sendF("c", 0xD9); u8g2_builtin.sendF("c", 0xF1);
-    u8g2_builtin.sendF("c", 0xDB); u8g2_builtin.sendF("c", 0x40);
-    u8g2_builtin.sendF("c", 0x8D); u8g2_builtin.sendF("c", 0x14);
+    u8g2_builtin.sendF("c", 0xD9); 
+    u8g2_builtin.sendF("c", 0xF1);
+    u8g2_builtin.sendF("c", 0xDB); 
+    u8g2_builtin.sendF("c", 0x40);
+    u8g2_builtin.sendF("c", 0x8D); 
+    u8g2_builtin.sendF("c", 0x14);
     insaneModeActive = true;
   } else {
-    u8g2_builtin.sendF("c", 0xD9); u8g2_builtin.sendF("c", 0x22);
-    u8g2_builtin.sendF("c", 0xDB); u8g2_builtin.sendF("c", 0x20);
-    u8g2_builtin.sendF("c", 0x8D); u8g2_builtin.sendF("c", 0x14);
+    u8g2_builtin.sendF("c", 0xD9); 
+    u8g2_builtin.sendF("c", 0x22);
+    u8g2_builtin.sendF("c", 0xDB); 
+    u8g2_builtin.sendF("c", 0x20);
+    u8g2_builtin.sendF("c", 0x8D); 
+    u8g2_builtin.sendF("c", 0x14);
     insaneModeActive = false;
   }
 }
@@ -59,7 +65,10 @@ void sendMSPRequest(uint8_t cmd) {
 
 void readMSPResponse() {
   while (Serial1.available() >= 6) {
-    if (Serial1.peek() != '$') { Serial1.read(); continue; }
+    if (Serial1.peek() != '$') { 
+      Serial1.read(); 
+      continue; 
+    }
     if (Serial1.read() == '$' && Serial1.read() == 'M' && Serial1.read() == '>') {
       uint8_t size = Serial1.read(); 
       uint8_t cmd = Serial1.read();
@@ -73,32 +82,51 @@ void readMSPResponse() {
         roll = angX / 10.0; 
         pitch = angY / 10.0; 
         heading = (float)head;
-        for (int i = 0; i < size - 6; i++) Serial1.read();
+        for (int i = 0; i < size - 6; i++) {
+          Serial1.read();
+        }
       } 
       else if (cmd == 109) { // ALTITUDE
         int32_t altCm = Serial1.read() | (Serial1.read() << 8) | (Serial1.read() << 16) | (Serial1.read() << 24);
-        altitude = altCm * 0.0328084; // cm to feet
-        for (int i = 0; i < size - 4; i++) Serial1.read();
+        altitude = altCm * 0.0328084; 
+        for (int i = 0; i < size - 4; i++) {
+          Serial1.read();
+        }
       }
       else if (cmd == 102) { // RAW IMU
-        for (int i = 0; i < 4; i++) Serial1.read();
+        for (int i = 0; i < 4; i++) {
+          Serial1.read();
+        }
         int16_t az = Serial1.read() | (Serial1.read() << 8); 
         currentG = (((float)az / ACCEL_1G) * 0.1) + (currentG * 0.9);
-        for (int i = 0; i < size - 6; i++) Serial1.read();
+        for (int i = 0; i < size - 6; i++) {
+          Serial1.read();
+        }
       } 
       else if (cmd == 110) { // ANALOG VOLTS
         vBat = ((Serial1.read() / 10.0) * 0.2) + (vBat * 0.8);
-        for (int i = 0; i < size - 1; i++) Serial1.read();
+        for (int i = 0; i < size - 1; i++) {
+          Serial1.read();
+        }
       } 
       else if (cmd == 105) { // RC CHANNELS
-        for (int i = 0; i < 8; i++) Serial1.read();
-        for (int i = 4; i < ARM_RC_CHANNEL; i++) { Serial1.read(); Serial1.read(); }
+        for (int i = 0; i < 8; i++) {
+          Serial1.read();
+        }
+        for (int i = 4; i < ARM_RC_CHANNEL; i++) { 
+          Serial1.read(); 
+          Serial1.read(); 
+        }
         armSwitchValue = Serial1.read() | (Serial1.read() << 8); 
         int bytesConsumed = 8 + ((ARM_RC_CHANNEL - 4) * 2) + 2; 
-        for (int i = 0; i < (size - bytesConsumed); i++) Serial1.read(); 
+        for (int i = 0; i < (size - bytesConsumed); i++) {
+          Serial1.read(); 
+        }
       } 
       else { 
-        for (int i = 0; i < size; i++) Serial1.read(); 
+        for (int i = 0; i < size; i++) {
+          Serial1.read(); 
+        }
       }
     }
   }
@@ -107,15 +135,15 @@ void readMSPResponse() {
 void setup() {
   esp_log_level_set("*", ESP_LOG_NONE);
 
-  // 1. LINK TO COMPUTER (USB Debugging)
+  // LINK TO COMPUTER (USB Debugging)
   // Ensure "USB CDC On Boot" is ENABLED in the Arduino Tools menu
   Serial.begin(115200); 
 
-  // 2. LINK TO FLIGHT CONTROLLER (Pins 20/21)
+  // LINK TO FLIGHT CONTROLLER (Pins 20/21)
   // This is your existing working FC connection
   Serial1.begin(115200, SERIAL_8N1, RX_FROM_FC, TX_TO_FC); 
 
-  // 3. LINK TO SLAVE (Cockpit Screens)
+  // LINK TO SLAVE (Cockpit Screens)
   // We use Serial0 but re-route it to different Pins
   // This prevents it from clashing with the FC on pins 20/21
   Serial0.begin(115200, SERIAL_8N1, SLAVE_RX, SLAVE_TX);
@@ -129,113 +157,200 @@ void setup() {
   u8g2_builtin.setContrast(20);
   applyHardwareBoost(false);
 
-  // This will now print to your Computer Console
-  Serial.println("MASTER ONLINE");
-  Serial.println("Pipe 1: USB (Debug)");
-  Serial.println("Pipe 2: Serial1 (FC on 20/21)");
-  Serial.print("Pipe 3: Serial0 - Slave TX on Master Pin: "); Serial.print(SLAVE_TX); Serial.print(" - Slave RX on Master Pin: "); Serial.println(SLAVE_RX);
-}
+  Serial.println("MASTER ONLINE - CHECKING FC...");
+  unsigned long bootCheckStart = millis();
+  
+  // Try for 2 seconds to find an FC
+  while (millis() - bootCheckStart < 2000) {
+    sendMSPRequest(108);
+    delay(100);
+    readMSPResponse();
+    if (sessionHasMSP) {
+      break;
+    }
+  }
 
-// void logStatus(String msg) {
-//   Serial.println("[PC] " + msg);    // Goes to your USB Monitor
-//   Serial0.println(msg);            // Goes to the Cockpit Screen
-// }
+  if (sessionHasMSP) {
+    isBenchMode = false;
+    Serial.println("MODE: FLIGHT (MSP DETECTED)");
+  } else {
+    isBenchMode = true;
+    Serial.println("MODE: BENCH (SIMULATING)");
+  }
+}
 
 void loop() {
   // INPUT HANDLING
   bool currentBootState = digitalRead(BOOT_BUTTON);
-  if (currentBootState == LOW && lastBootState == HIGH) { buttonDownTime = millis(); isLongPress = false; }
+  
+  if (currentBootState == LOW && lastBootState == HIGH) { 
+    buttonDownTime = millis(); 
+    isLongPress = false; 
+  }
   else if (currentBootState == HIGH && lastBootState == LOW && !isLongPress) {
-    if (manualMode) { manualContrast = (manualContrast >= 255) ? 20 : manualContrast + 40; } else { txMax = !txMax; }
-  } else if (currentBootState == LOW && (millis() - buttonDownTime > 1000) && !isLongPress) { manualMode = !manualMode; isLongPress = true; }
+    if (manualMode) { 
+      manualContrast = (manualContrast >= 255) ? 20 : manualContrast + 40; 
+    } else { 
+      txMax = !txMax; 
+    }
+  } 
+  else if (currentBootState == LOW && (millis() - buttonDownTime > 1000) && !isLongPress) { 
+    manualMode = !manualMode; 
+    isLongPress = true; 
+  }
   lastBootState = currentBootState;
 
-  // 2. DATA POLLING
-  if (millis() - lastRequest > 50) { 
-    sendMSPRequest(108); 
-    sendMSPRequest(109); 
-    sendMSPRequest(105); 
-    sendMSPRequest(102); 
-    sendMSPRequest(110); 
-    lastRequest = millis(); 
-  }
-  readMSPResponse();
-
-  // PHYSICS & SIMULATION
-  bool isArmed = (armSwitchValue > 1800);
-  bool warActive = isArmed || txMax;
-  
-  if (isArmed && vBat < VOLT_THRESHOLD) {
-    if (lowVoltTimer == 0) lowVoltTimer = millis();
-    if (millis() - lowVoltTimer > BATT_DEBOUNCE) showLowBatText = true;
-  } else { lowVoltTimer = 0; showLowBatText = false; }
-
-  int targetContrast = manualMode ? manualContrast : (warActive ? 255 : 20);
-  if (abs(currentContrast - targetContrast) > 1) {
-    currentContrast += (targetContrast - currentContrast) * ((targetContrast > currentContrast) ? 0.10 : 0.25); 
-    u8g2_builtin.setContrast((int)currentContrast);
-    if (currentContrast > 210 && !insaneModeActive) applyHardwareBoost(true);
-    if (currentContrast < 90 && insaneModeActive) applyHardwareBoost(false);
-  }
-
-  // PHYSICS & GHOST SIMULATION
-  currentlyReceiving = (millis() - lastDataTime < 1000);
-  
-  if (!sessionHasMSP || !currentlyReceiving) {
+  // DATA ACQUISITION (LOCKED MODE)
+  if (isBenchMode) {
     float t = millis() / 1000.0;
     pitch = sin(t * 0.5) * 20.0; 
     roll = cos(t * 0.8) * 45.0; 
     altitude = 500 + (sin(t * 0.2) * 100);
-    heading += 0.5; if (heading >= 360) heading = 0;
-  } else {
-    float pDelta = (pitch - lastPitch); float rDelta = (roll - lastRoll);
-    filteredX = ((rDelta * -LEAD_SENSITIVITY) * 0.12) + (filteredX * 0.88);
-    filteredY = ((pDelta * LEAD_SENSITIVITY) * 0.12) + (filteredY * 0.88);
-    lastPitch = pitch; lastRoll = roll;
+    heading += 0.5; 
+    if (heading >= 360) {
+      heading = 0;
+    }
+    vBat = 16.2;
+    currentG = 1.0;
+    currentlyReceiving = false; 
+  } 
+  else {
+    if (millis() - lastRequest > 50) { 
+      sendMSPRequest(108); 
+      sendMSPRequest(109); 
+      sendMSPRequest(105); 
+      sendMSPRequest(102); 
+      sendMSPRequest(110); 
+      lastRequest = millis(); 
+    }
+    readMSPResponse();
+    currentlyReceiving = (millis() - lastDataTime < 1000);
   }
 
-  // THE 3-CHAR TAGGED BROADCAST ---
-  // Broadcast frequency synced to gauge refresh
+  // SHARED PHYSICS
+  bool isArmed = (armSwitchValue > 1800);
+  bool warActive = isArmed || txMax;
+  
+  float pDelta = (pitch - lastPitch); 
+  float rDelta = (roll - lastRoll);
+  
+  filteredX = ((rDelta * -LEAD_SENSITIVITY) * 0.12) + (filteredX * 0.88);
+  filteredY = ((pDelta * LEAD_SENSITIVITY) * 0.12) + (filteredY * 0.88);
+  
+  lastPitch = pitch; 
+  lastRoll = roll;
+
+  // POWER & CONTRAST
+  if (isArmed && vBat < VOLT_THRESHOLD) {
+    if (lowVoltTimer == 0) {
+      lowVoltTimer = millis();
+    }
+    if (millis() - lowVoltTimer > BATT_DEBOUNCE) {
+      showLowBatText = true;
+    }
+  } else { 
+    lowVoltTimer = 0; 
+    showLowBatText = false; 
+  }
+
+  int targetContrast = manualMode ? manualContrast : (warActive ? 255 : 20);
+  
+  if (abs(currentContrast - targetContrast) > 1) {
+    float step = (targetContrast > currentContrast) ? 0.10 : 0.25;
+    currentContrast += (targetContrast - currentContrast) * step; 
+    u8g2_builtin.setContrast((int)currentContrast);
+    
+    if (currentContrast > 210 && !insaneModeActive) {
+      applyHardwareBoost(true);
+    }
+    if (currentContrast < 90 && insaneModeActive) {
+      applyHardwareBoost(false);
+    }
+  }
+
+  // THE 3-CHAR TAGGED BROADCAST
   if (millis() - lastBroadcast > 50) {
-    Serial0.print("ROL:"); Serial0.print(roll); Serial0.print(",");
-    Serial0.print("PIT:"); Serial0.print(pitch); Serial0.print(",");
-    Serial0.print("HED:"); Serial0.print(heading); Serial0.print(",");
-    Serial0.print("ALT:"); Serial0.print(altitude);Serial0.print(",");
-    Serial0.print("BAT:"); Serial0.print(vBat); Serial0.print(",");
-    Serial0.print("GFO:"); Serial0.print(currentG);Serial0.print(",");
-    Serial0.print("WAR:"); Serial0.println(warActive ? 1 : 0); // 1 = High Brightness, 0 = Dim
+    Serial0.print("ROL:"); 
+    Serial0.print(roll); 
+    Serial0.print(",");
+    
+    Serial0.print("PIT:"); 
+    Serial0.print(pitch); 
+    Serial0.print(",");
+    
+    Serial0.print("HED:"); 
+    Serial0.print(heading); 
+    Serial0.print(",");
+    
+    Serial0.print("ALT:"); 
+    Serial0.print(altitude);
+    Serial0.print(",");
+    
+    Serial0.print("BAT:"); 
+    Serial0.print(vBat); 
+    Serial0.print(",");
+    
+    Serial0.print("GFO:"); 
+    Serial0.print(currentG);
+    Serial0.print(",");
+    
+    Serial0.print("WAR:"); 
+    Serial0.println(warActive ? 1 : 0);
+    
     lastBroadcast = millis();
   }
 
   // HUD RENDERING
   float distSq = (filteredX * filteredX) + (filteredY * filteredY);
-  bool ledOn = (distSq < 1.5) || (showLowBatText && (millis() % 200 < 100));
+  bool blink = (millis() % 200 < 100);
+  bool ledOn = (distSq < 1.5) || (showLowBatText && blink);
   digitalWrite(LED_BLUE, ledOn ? LOW : HIGH);
 
   u8g2_builtin.firstPage();
   do {
     u8g2_builtin.setFont(u8g2_font_04b_03_tr);
-    const char* sym = currentlyReceiving ? (warActive ? "*" : "+") : "-";
-    u8g2_builtin.drawStr(2, 5, sym); u8g2_builtin.drawStr(66, 5, sym);
-    u8g2_builtin.drawStr(2, 39, sym); u8g2_builtin.drawStr(66, 39, sym);
-
-    // K-14 Sight Reticle
-    u8g2_builtin.drawCircle(36, 20, 17); u8g2_builtin.drawCircle(36, 20, 18);
-    int curX = 36 + (int)filteredX; int curY = 20 + (int)filteredY;
-    u8g2_builtin.drawBox(curX - 1, curY - 1, 3, 3);
-    for (int i = 0; i < 6; i++) {
-      float a = i * 1.047;
-      u8g2_builtin.drawBox(curX + (cos(a) * 12) - 1, curY + (sin(a) * 12) - 1, 2, 2);
+    
+    const char* sym = "-";
+    if (!isBenchMode) {
+      if (currentlyReceiving) {
+        sym = warActive ? "*" : "+";
+      } else {
+        sym = "X";
+      }
     }
     
-    // G-Meter
+    u8g2_builtin.drawStr(2, 5, sym); 
+    u8g2_builtin.drawStr(66, 5, sym);
+    u8g2_builtin.drawStr(2, 39, sym); 
+    u8g2_builtin.drawStr(66, 39, sym);
+
+    u8g2_builtin.drawCircle(36, 20, 17); 
+    u8g2_builtin.drawCircle(36, 20, 18);
+    
+    int curX = 36 + (int)filteredX; 
+    int curY = 20 + (int)filteredY;
+    
+    u8g2_builtin.drawBox(curX - 1, curY - 1, 3, 3);
+    
+    for (int i = 0; i < 6; i++) {
+      float a = i * 1.047;
+      int bx = curX + (cos(a) * 12) - 1;
+      int by = curY + (sin(a) * 12) - 1;
+      u8g2_builtin.drawBox(bx, by, 2, 2);
+    }
+    
     u8g2_builtin.drawLine(0, 10, 0, 30);
-    u8g2_builtin.drawHLine(0, constrain(20 - (int)((currentG - 1.0) * 8.0), 10, 30), 3);
+    int gY = constrain(20 - (int)((currentG - 1.0) * 8.0), 10, 30);
+    u8g2_builtin.drawHLine(0, gY, 3);
 
     if (showLowBatText) {
-      char vBuf[6]; dtostrf(vBat, 4, 1, vBuf);
-      u8g2_builtin.drawStr(25, 39, "LOW "); u8g2_builtin.drawStr(45, 39, vBuf);
+      char vBuf; 
+      dtostrf(vBat, 4, 1, vBuf);
+      u8g2_builtin.drawStr(25, 39, "LOW "); 
+      u8g2_builtin.drawStr(45, 39, vBuf);
     }
-    if (manualMode) u8g2_builtin.drawStr(28, 5, "MAN");
+    if (manualMode) {
+      u8g2_builtin.drawStr(28, 5, "MAN");
+    }
   } while (u8g2_builtin.nextPage());
 }
